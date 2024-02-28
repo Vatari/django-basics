9304e697c186a610f61da19fd36039a19595904e
vatari <majestic13@gmail.com>
2024-02-28 07:59:08 +0200

    exam prep

---
+++ exam_prep/exam_prep/albums/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from exam_prep.albums.views import CreateAlbumView, DetailAlbumView, EditAlbumView, DeleteAlbumView
+
+urlpatterns = (
+    path("add/", CreateAlbumView.as_view(), name="create_album"),
+    path("<int:pk>/details", DetailAlbumView.as_view(), name="details_album"),
+    path("<int:pk>/edit", EditAlbumView.as_view(), name="edit_album"),
+    path("<int:pk>/delete", DeleteAlbumView.as_view(), name="delete_album")
+)
